a1ae22a6aa2449d1a3a3dad09e0ca9d949536c9d
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-03-09 00:51:43 +0500

    Add files via upload

---
+++ 12.Greeting/App12.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ['Anoushey', 'Theniyat', 'Asra', 'Fakiha'];
+const message = 'welome to Typescript Class:  ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
+;
---
+++ 12.Greeting/App12.ts
@@ -0,0 +1,9 @@
+let names : string [] = ['Anoushey','Theniyat','Asra','Fakiha'] ;
+
+const message : string = 'welome to Typescript Class:  ' ;
+
+for(let i=0; i<names.length;i++) {
+
+   console.log(message + names [i]);
+
+};
