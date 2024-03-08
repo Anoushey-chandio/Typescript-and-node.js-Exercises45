56bc90444115435d1154063721dbdffb46a32f8d
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-03-09 00:58:01 +0500

    Add files via upload

---
+++ 14.Guest _List/Guest_List14.js
@@ -0,0 +1,6 @@
+"use strict";
+let Guest_List = ['Sajal Ali ', 'Maya Ali', 'Mahira Khan'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Miss. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you !');
+}
+;
