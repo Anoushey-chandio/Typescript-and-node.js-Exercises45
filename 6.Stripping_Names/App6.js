d9dfd6f27dfd9fe5712eafadc2edc9cdff4cfa9b
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-03-09 00:36:18 +0500

    Add files via upload

---
+++ 6.Stripping_Names/App6.js
@@ -0,0 +1,5 @@
+"use strict";
+const person_Name = '   \n\tAnoushey Chandio\t\n    ';
+console.log(person_Name);
+const without_whitespace = person_Name.trim();
+console.log(without_whitespace);
---
+++ 6.Stripping_Names/App6.ts
@@ -0,0 +1,7 @@
+const person_Name :string = '   \n\tAnoushey Chandio\t\n    ';
+
+console.log( person_Name);
+
+const without_whitespace :string = person_Name.trim();
+
+console.log( without_whitespace);
