1c6ed2dfe5a80b4d31ff55be6e8bff8ce5662e93
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-03-09 00:55:09 +0500

    Add files via upload

---
+++ 13.Your _own_Array/App13.js
@@ -0,0 +1,6 @@
+"use strict";
+let transportation = ['Motorcycle ', 'Car', 'Train', 'cycle '];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('I Would Like to Own a ' + transportation[i]);
+}
+;
---
+++ 13.Your _own_Array/App13.ts
@@ -0,0 +1,9 @@
+let transportation : string [] = ['Motorcycle ','Car','Train','cycle '] ;
+
+
+
+for(let i=0; i<transportation.length; i++) {
+
+   console.log('I Would Like to Own a ' + transportation[i]);
+
+};
