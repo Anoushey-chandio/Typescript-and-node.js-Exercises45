79daa5eeb2fc3e1f21434a4d96f9a0669096639c
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-06-01 22:48:25 +0500

    Add files via upload

---
+++ 20.Empty_Array/empty_Array20.js
@@ -0,0 +1,8 @@
+"use strict";
+let items = ['Pakistan', 'Karachi', 'urdu'];
+console.log(items);
+// let item2 : string[] =[]
+// item2.push('Turkey');
+// item2.push('Istanbul');
+// item2.push('Italy ');
+// console.log(item2);
---
+++ 20.Empty_Array/empty_Array20.ts
@@ -0,0 +1,9 @@
+let items : string []=['Pakistan', 'Karachi', 'urdu'];
+
+let item2 : string[] =[]
+
+item2.push('Turkey');
+item2.push('Istanbul');
+item2.push('Italy ');
+
+console.log(item2);
