eb53447ba43190ee70a2348ff0ddc9160d9353b5
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-06-01 22:52:45 +0500

    Add files via upload

---
+++ 22.International_Error/international_Error22.js
@@ -0,0 +1,9 @@
+"use strict";
+//if you have not received an array index error in one of your programs yet , try to make one happen.
+//Create Array
+let newArray = ['Tom cruise', 'Bill Gates', 'Justin Trudeau'];
+//Accessing error by index
+console.log(newArray[5]);
+//make sure you correct the error before closing the program 
+//correct the error
+console.log(newArray);
---
+++ 22.International_Error/international_Error22.ts
@@ -0,0 +1,13 @@
+//if you have not received an array index error in one of your programs yet , try to make one happen.
+
+//Create Array
+let newArray : string [] =['Tom cruise','Bill Gates', 'Justin Trudeau'];
+//Accessing error by index
+
+console.log(newArray[5]);
+
+//make sure you correct the error before closing the program 
+
+//correct the error
+ console.log(newArray);
+
