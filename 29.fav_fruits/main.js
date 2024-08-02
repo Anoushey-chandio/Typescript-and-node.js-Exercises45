b72a1acf3fa5470a7181bc43397f3df577591e39
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-08-02 15:44:00 +0500

    Add files via upload

---
+++ 29.fav_fruits/main.js
@@ -0,0 +1,21 @@
+"use strict";
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series
+// Make a array of your three favorite fruits and call it favorite_fruits] 
+let fav_fruits = ['apple', 'banana', 'mango'];
+if (fav_fruits.includes('apple')) {
+    console.log('i really like apple!');
+}
+if (fav_fruits.includes('banana')) {
+    console.log('i really like banana!');
+}
+if (fav_fruits.includes('mango')) {
+    console.log('i really like mango!');
+}
+if (fav_fruits.includes('strawberry')) {
+    console.log('i really like strawberry!');
+}
+if (fav_fruits.includes('orange')) {
+    console.log('i really like orange!');
+}
+//. Write five if statements. Each should check whether a certain kind of fruit is in your array.
+// If the fruit is in your array, the if block should print a statement, such as You really like bananas!
