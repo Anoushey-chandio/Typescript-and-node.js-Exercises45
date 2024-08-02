e0fdee46689479533b8e8cce6c5d53bfb035071e
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-08-02 16:11:04 +0500

    Add files via upload

---
+++ 43.unchanged_magician/main.js
@@ -0,0 +1,26 @@
+"use strict";
+// Unchanged Magicians: Start with your work from Exercise 40. call the function make great()
+// with a copy of the array of magicians' names. Because the original array will be unchanged, 
+//return the new array and store it in a separate array, Call show_magicians()
+// with each array to show that you have one array of the original names and one array
+// with the Great added to each magician's name.
+let magician = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
+function copyArray(arr) {
+    return [...arr];
+}
+function make_great(magicianarray) {
+    for (let i = 0; i < magicianarray.length; i++) {
+        magicianarray[i] = 'the great ' + magicianarray[i];
+    }
+}
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+const copyMagicianArray = copyArray(magician);
+make_great(copyMagicianArray);
+console.log('\n\nThis is my orginal array..');
+show_magicians(magician);
+console.log('\n\nThis is my modified copy of the array...');
+show_magicians(copyMagicianArray);
