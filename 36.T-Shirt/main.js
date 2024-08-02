49c21a82acbdf4b9f6c932aa4c5e284b03684a10
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2024-08-02 15:59:22 +0500

    Add files via upload

---
+++ 36.T-Shirt/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// T-Shirt: write a function called make shirt() that accepts a size and the text of a message
+// that should be printed on the shirt. The function should print a sentence summarizing the size of the 
+//shirt and the message printed on it. Call the function.
+function makeShirt(size, text) {
+    console.log(`Creating a ${size} shirt with the message: ${text} `);
+}
+makeShirt('large', 'hello world');
+makeShirt('medium', 'Hi cutie');
+makeShirt('small', 'never give up ');
---
+++ 36.T-Shirt/main.ts
@@ -0,0 +1,13 @@
+// T-Shirt: write a function called make shirt() that accepts a size and the text of a message
+// that should be printed on the shirt. The function should print a sentence summarizing the size of the 
+//shirt and the message printed on it. Call the function.
+
+function makeShirt(size:string, text:string){
+    console.log(`Creating a ${size} shirt with the message: ${text} `)
+}
+
+makeShirt('large', 'hello world');
+
+makeShirt('medium', 'Hi cutie');
+
+makeShirt('small', 'never give up ');
